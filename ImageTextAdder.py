07d5c9ec585fc020b6be6c4ada1b06adc7a7dc44
chetkauskas glen <mansonnne@gmail.com>
2024-06-01 23:25:09 +1000

    connected 3 modules together

--- ImageTextAdder.py
+++ ImageTextAdder.py
@@ -5,14 +5,14 @@ class ImageTextAdder:
     def __init__(self, font_path):
         self.font_path = font_path
     
-    def add_text(self, image_path, text):
-        # Извлекаем разрешение из имени файла
-        filename = os.path.basename(image_path)
-        resolution = filename.split('.')[0]
-        width, height = map(int, resolution.split('_'))
-
-        # Открываем изображение
+    def get_size(self, image_path):
         image = Image.open(image_path)
+        width, height = image.size
+        return width, height, image
+
+    def add_text(self, image_path, text):
+        # Получаем размеры изображения
+        width, height, image = self.get_size(image_path)
         draw = ImageDraw.Draw(image)
 
         # Выбираем размер шрифта в зависимости от разрешения
@@ -20,32 +20,76 @@ class ImageTextAdder:
         font = ImageFont.truetype(self.font_path, font_size)
 
         # Определяем позицию для текста в зависимости от разрешения
-        position = self.get_text_position(width, height)
+        position = self.create_global_container_position(width, height)
+
+        color = (238, 17, 51)
 
         # Цвет текста в формате RGB
-        color = (235, 64, 52)
+        # dark_blue_color = (0, 136, 187)
+        # blue_color = (0, 187, 238)
+        # red_color = (238, 17, 51)
+        # light_red_color = (255, 90, 90)
+        # orange_color = (255, 187, 68)
+        # sand_color = (255, 204, 136)
+        # black_color = (0, 0, 0)
+        # gray_color = (85, 85, 85)
 
         # Добавляем текст на изображение
         draw.text(position, text, font=font, fill=color)
 
+        if width <= height:
+            self.add_rectangle(draw, position, width*0.8, height*0.4, outline_color="black", fill_color=None)
+        else:
+            self.add_rectangle(draw, position, width*0.55, height*0.8, outline_color="black", fill_color=None)
+
         # Сохраняем измененное изображение
-        output_path = f"{filename}"
+        output_path = f"out_{os.path.basename(image_path)}"
+        output_folder = "output"
+        output_folder_path = os.path.join(os.getcwd(), output_folder)
+        output_path = os.path.join(output_folder_path,  f"{os.path.basename(image_path)}")
         image.save(output_path)
         print(f"Saved image with text at: {output_path}")
 
-    def get_text_position(self, width, height):
-        # Пример логики определения позиции текста
-        if width >= 1920 and height >= 1080:
-            return (width // 10, height // 10)  # Верхний левый угол
-        elif width >= 1280 and height >= 720:
-            return (width // 2, height // 2)  # Центр
-        else:
-            return (width // 4, height // 4)  # Середина верхнего левого квадранта
+    def add_rectangle(self, draw, position, rect_width, rect_height, outline_color, fill_color=None):
+        x, y = position
+        shape = [(x, y), (x + rect_width, y + rect_height)]
+        draw.rectangle(shape, outline=outline_color, fill=fill_color, width=5)
+
+
+    def create_global_container_position(self, width, height):
+        if ((width == 1748) and (height == 2480)) or ((width == 3507) and (height == 4960)) or ((width == 700) and (height == 1080)):
+            x = width*0.1; y = height*0.55
+            return x,y
+        elif ((width == 1084) and (height == 585)) or ((width == 1200) and (height == 520)) or ((width == 1200) and (height == 593)) or ((width == 1920) and (height == 1080)):
+            x = width*0.055; y = height*0.1
+            return x,y
+        
+    def process_images_in_folder(self, folder_path, text):
+        for filename in os.listdir(folder_path):
+            image_path = os.path.join(folder_path, filename)
+            self.add_text(image_path, text)
+
 
 # Пример использования
-font_path = "font.ttf"
-image_paths = ["297_420.png"]
+font_path = "arialmt.ttf"
+folder_path = "output/"
 
 adder = ImageTextAdder(font_path)
-for image_path in image_paths:
-    adder.add_text(image_path, "AHAHAHAKATON")
+adder.process_images_in_folder(folder_path, "AHAHAHAKATON")
+# image_paths = ["1748_2480.png", "3507_4960.png", "700_1080.png", "1084_585.png", "1200_520.png", "1200_593.png", "1920_1080.png"]
+
+# adder = ImageTextAdder(font_path)
+# # for image_path in image_paths:
+# adder.add_text(image_paths[0], "AHAHAHAKATON")
+# adder.add_text(image_paths[1], "AHAHAHAKATON")
+# adder.add_text(image_paths[2], "AHAHAHAKATON")
+# adder.add_text(image_paths[3], "AHAHAHAKATON")
+# adder.add_text(image_paths[4], "AHAHAHAKATON")
+# adder.add_text(image_paths[5], "AHAHAHAKATON")
+# adder.add_text(image_paths[6], "AHAHAHAKATON")
+
+
+#цвет должен выбираться в зависимости от переданного параметра
+
+# у Ильи должна быть функция которая будет передовать значения загловока подзаголовка и кнопки
+# а я будет и считывать в переменные: Text1, Text2, Text3 = get_text()
---
+++ inerface copy.py
@@ -0,0 +1,147 @@
+import tkinter as tk
+from dkd import *
+from ImageTextAdder import *
+
+root = tk.Tk()  # создание окна
+root.title("IGKA")  # заголовок окна
+root.geometry("1024x800")  # установка размеров
+root.resizable(height=True, width=True)  # закрепление размеров
+
+# Configure the grid to have columns and multiple rows that expand
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_rowconfigure(2, weight=1)
+root.grid_rowconfigure(3, weight=1)
+root.grid_rowconfigure(4, weight=1)
+root.grid_rowconfigure(5, weight=1)
+root.grid_rowconfigure(6, weight=1)
+root.grid_rowconfigure(7, weight=1)
+root.grid_rowconfigure(8, weight=1)
+root.grid_rowconfigure(9, weight=1)
+root.grid_rowconfigure(10, weight=1)
+root.grid_rowconfigure(11, weight=1)
+root.grid_rowconfigure(12, weight=1)
+root.grid_rowconfigure(13, weight=1)
+
+# Create a label widget
+label = tk.Label(root, text="1 Billion Dollar App")
+label.grid(row=0, column=0, pady=20, sticky=tk.NS)
+
+result_label = tk.Label(root, text="")
+result_label.grid(row=1, column=0, pady=20, sticky=tk.NS)
+
+# Create an entry widget
+entry = tk.Entry(root, width=40, fg='grey')
+entry.insert(0, "Введите ваш запрос")
+entry.grid(row=3, column=0, pady=20, sticky=tk.NS)
+
+def on_entry_click(event):
+    if entry.get() == "Введите ваш запрос":
+        entry.delete(0, "end") # delete all the text in the entry
+        entry.insert(0, '') #Insert blank for user input
+        entry.config(fg='black')
+
+entry.bind('<FocusIn>', on_entry_click)
+
+# Add a label above the checkboxes
+checkbox_label = tk.Label(root, text="Выберите форматы, в которых необходимо получить изображение")
+checkbox_label.grid(row=4, column=0, pady=20, sticky=tk.NS)
+
+# Create BooleanVar variables for checkboxes
+checkbox_vars = [tk.BooleanVar() for _ in range(7)]
+checkbox_labels = [ "Информационные доски (Формат А3, 297*420мм)", 
+                    "Тейбл-тенты (Формат А4, 148*210мм)",
+                    "Экраны блокировки ПК (FullHD 1920*1080 px)", 
+                    "Кликабельный баннер, 1200*593 px",
+                    "Кликабельный баннер, 1200*520 px", 
+                    "Иллюстрация к новости, 700*1080 px", 
+                    "Иллюстрация на анонс, 1084х585 px"]
+
+# Options for dropdown menus
+dropdown_options_first = ["PNG", "JPG", "PDF", "PPTX"]
+dropdown_options_second = ["PNG", "JPG", "PDF"]
+dropdown_options_others = ["PNG", "JPG"]
+
+# Add label for color selection dropdown
+color_label = tk.Label(root, text="Выбор цвета")
+color_label.grid(row=4, column=1, pady=20, sticky=tk.N)
+
+# Options for color dropdown menu
+color_options = ["Цвет 1", "Цвет 2", "Цвет 3", "Цвет 4", "Цвет 5", "Цвет 6", "Цвет 7", "Цвет 8"]
+
+# Create color dropdown menu
+selected_color = tk.StringVar(root)
+selected_color.set(color_options[0])  # set default value
+color_dropdown = tk.OptionMenu(root, selected_color, *color_options)
+color_dropdown.grid(row=5, column=1, pady=5, sticky=tk.W, padx=50)
+
+# Create a list to store dropdown menus' StringVar and corresponding options list
+dropdown_vars = []
+dropdown_options_list = []
+
+# Create checkboxes and dropdown menus
+for i, (var, label) in enumerate(zip(checkbox_vars, checkbox_labels), start=6):
+    checkbox = tk.Checkbutton(root, text=label, variable=var, command=lambda: check_checkbox_state())
+    checkbox.grid(row=i, column=0, pady=5, sticky=tk.W, padx=50)
+
+    # Create dropdown menu
+    selected_option = tk.StringVar(root)
+    if i == 7:  # First dropdown menu
+        selected_option.set(dropdown_options_first[0])  # set default value
+        dropdown = tk.OptionMenu(root, selected_option, *dropdown_options_first)
+        dropdown_options_list.append(dropdown_options_first)
+    elif i == 6:  # Second dropdown menu
+        selected_option.set(dropdown_options_second[0])  # set default value
+        dropdown = tk.OptionMenu(root, selected_option, *dropdown_options_second)
+        dropdown_options_list.append(dropdown_options_second)
+    else:  # Other dropdown menus
+        selected_option.set(dropdown_options_others[0])  # set default value
+        dropdown = tk.OptionMenu(root, selected_option, *dropdown_options_others)
+        dropdown_options_list.append(dropdown_options_others)
+    
+    dropdown.grid(row=i, column=1, pady=5, sticky=tk.W, padx=50)
+    dropdown_vars.append(selected_option)  # Add the dropdown variable to the list
+
+def check_checkbox_state():
+    if any(var.get() for var in checkbox_vars):
+        process_button.config(state=tk.NORMAL)
+    else:
+        process_button.config(state=tk.DISABLED)
+
+# Function to handle text processing
+def process_text():
+    input_text = entry.get()
+    checkbox_values = []
+    for idx, (var, dropdown_var) in enumerate(zip(checkbox_vars, dropdown_vars)):
+        status = int(var.get())
+        try:
+            format_index = dropdown_options_list[idx].index(dropdown_var.get()) + 1
+        except ValueError:
+            format_index = 0  # если формат не найден, установить значение по умолчанию
+        checkbox_values.append(f"{status}{format_index}")
+    selected_color_index = color_options.index(selected_color.get()) + 1
+    creation(input_text,checkbox_values,selected_color_index)
+#####################
+    font_path = "arialmt.ttf"
+    folder_path = "output/"
+
+    adder = ImageTextAdder(font_path)
+    adder.process_images_in_folder(folder_path, "AHAHAHAKATON")
+
+
+    result_process.config(text=f"Processed: {input_text}, Checkboxes: {checkbox_values}, Color: {selected_color_index}")
+
+# Create a process button
+process_button = tk.Button(root, text="Process Text", command=process_text, state=tk.DISABLED)
+process_button.grid(row=12, column=0, pady=20, sticky=tk.N)
+
+result_process = tk.Label(root, text="")
+result_process.grid(row=13, column=0, pady=20, sticky=tk.N)
+
+# Initial check to set the button state
+check_checkbox_state()
+
+root.mainloop()
